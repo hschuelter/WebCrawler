25a6852d63c8e011cc358aff554254f62779002d
Arthur Schuelter <arthur.schuelter@gmail.com>
2020-01-16 20:27:13 -0300

    IEEEX vamo

--- Scrapy/projeto_teste/projeto_teste/spiders/ieeex.py
+++ Scrapy/projeto_teste/projeto_teste/spiders/ieeex.py
@@ -12,30 +12,53 @@ class IEEEX_Spider(scrapy.Spider):
         # //a[@_ngcontent-c32]/span/text()
 
         print("Authors: ")
-        for author_raw in response.xpath('//a[@_ngcontent-c32]/span/'):
+        for author_raw in response.xpath('//a[@_ngcontent-c7]/span'):
             content = author_raw.xpath('./text()').extract_first()
             authors.append(content)
 
         return authors
 
     def get_title(self, response):
-        title = ""
-        for author_raw in response.xpath('//title'):
-            title = author_raw.xpath('./text()').extract_first()
-            break
+        title = []
+        for title_raw in response.xpath("//script[@type='text/javascript']"):
+            # if("global.document.metadata" in title_raw.extract_first() ):
+            helper = title_raw.xpath("./text()").extract_first()
+            if( (helper is not None) and ("global.document.metadata" in helper) ):
+                title = helper.split('\n')
+                # print(title)
+                break
 
-        return title
+        # helper = helper.split('\n')
+        while('' in title):
+            title.remove('')
 
+        title_scraped = ""
+        for e in title:
+            for es in e.split(','):
+                if( '"title"' in es ):
+                    title_scraped = es
+                    break
+
+            if(title_scraped != ""):
+                break
+
+        return title_scraped.split(':')[1]
 
     ##############################################
 
     def parse(self, response):
+
+        # print("###############")
+        # print(response.body)
+        # print("###############")
         
-        article_authors = self.get_authors(response)
-        print(article_authors)
+        # article_authors = self.get_authors(response)
+        # print(article_authors)
 
         article_title = self.get_title(response)
-        print(article_title)
+        # print(article_title)
+
+        print("BBBBBBBBBBBBB")
 
         # article_abstract = self.get_abstract(response)
         # article_conference = self.get_conference(response)
@@ -52,10 +75,10 @@ class IEEEX_Spider(scrapy.Spider):
         # article_references = self.get_references(response)
 
         ##############
-        # print("=========================")
+        print("=========================")
         # print("Authors: ", end="")
         # print(article_authors)
-        # print("Title: \"" + article_title + "\"")
+        print("Title: \"" + article_title + "\"")
         # print("Abstract: \"" + article_abstract + "\"")
         # print("Conference: \"" + article_conference + "\"")
         # print("Date: \"" + article_date + "\"")
@@ -65,7 +88,7 @@ class IEEEX_Spider(scrapy.Spider):
         # print("ISBN: \"" + article_isbn + "\"")
         # print("Total Citations: \"" + article_total_citations + "\"")
         # print("Total Downloads: \"" + article_total_downloads + "\"")
-        # print("=========================")
+        print("=========================")
         # # for r in article_references:
         # #     print(">> " + r)
         # print("Num referencias: " + str(len(article_references)))
